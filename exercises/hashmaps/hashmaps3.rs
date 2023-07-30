4a06bb8d8703d17f8a7a4ab83c6343a27b5bd267
SturdyBokeh <111076446+SturdyBokeh@users.noreply.github.com>
2023-07-30 16:44:45 +0100

    up to errors3.rs

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,11 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Debug)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
@@ -28,10 +28,10 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
 
-    for r in results.lines() {
-        let v: Vec<&str> = r.split(',').collect();
-        let team_1_name = v[0].to_string();
-        let team_1_score: u8 = v[2].parse().unwrap();
+    for r in results.lines() { // "England,France,4,2\n"
+        let v: Vec<&str> = r.split(',').collect(); // [England, France, 4, 2]
+        let team_1_name = v[0].to_string(); // String("England")
+        let team_1_score: u8 = v[2].parse().unwrap(); // "4":String => 4:u8
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
         // TODO: Populate the scores table with details extracted from the
@@ -39,6 +39,14 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let team_1 = scores.entry(team_1_name).or_insert(Team { goals_scored: (0), goals_conceded: (0) });
+        team_1.goals_conceded += team_2_score;
+        team_1.goals_scored += team_1_score;
+
+        let team_2 = scores.entry(team_2_name).or_insert(Team { goals_scored: (0), goals_conceded: (0) });
+        team_2.goals_conceded += team_1_score;
+        team_2.goals_scored += team_2_score;
+
     }
     scores
 }
